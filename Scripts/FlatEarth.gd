f499116eef0c8d57a650769352fc6a9b5ab8c4cd
Keinplanwieichheise <kawihdertroll@gmail.com>
2022-11-06 04:59:43 +0100

    fix and new house

--- Scripts/FlatEarth.gd
+++ Scripts/FlatEarth.gd
@@ -8,11 +8,9 @@ func _ready():
 	$FlatEarth.rotation_degrees.y = int(rand_range(0,360))
 	rotation_degrees = $"../%Player/SpringArm".rotation_degrees
 	translation = $"../%Player".translation + Vector3(0,1.9,0.15).rotated(Vector3.UP, $"../%Player/playerModel".rotation.y)
-
 	add_force(-transform.basis.z * 15, translation)
 
 func _physics_process(delta):
 	$FlatEarth.rotation_degrees.y += 10
-
 	if $Timer.is_stopped():
 		queue_free()
